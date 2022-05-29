9888a0d78f757b2355fc6abcba74f0b4c9d0939c
Aibek <101047236+t4iler@users.noreply.github.com>
2022-05-29 22:15:54 +0600

    Update image_converter.py

--- image_converter.py
+++ image_converter.py
@@ -3,10 +3,10 @@ from tkinter import filedialog
 from PIL import Image
 
 root = tk.Tk()
-canvas01 = tk.Canvas(root, width=450, height=400, bg='azure3', relief='raised')
+canvas01 = tk.Canvas(root, width=450, height=400, bg='pink1', relief='raised')
 canvas01.pack()
 
-labl = tk.Label(root, text='Image Converter', bg='azure3')
+labl = tk.Label(root, text='Image Converter', bg='pink3', fg='white')
 labl.config(font=('georgia', 30))
 canvas01.create_window(240, 150, window=labl)
 
@@ -15,7 +15,7 @@ def PNG():
     import_file_path = filedialog.askopenfilename()
     iml = Image.open(import_file_path)
 
-browse_png = tk.Button(text='Select PNG file', command=PNG, bg='royalblue', fg='black', font=('georgia', 14, 'bold'))
+browse_png = tk.Button(text='Select PNG file', command=PNG, bg='purple1', fg='white', font=('georgia', 14, 'bold'))
 canvas01.create_window(230, 220, window=browse_png)
 
 def convert():
@@ -23,6 +23,6 @@ def convert():
     export_file_path = filedialog.asksaveasfilename(defaul='.jpg')
     iml.save(export_file_path)
 
-savebutton = tk.Button(text = 'Convert PNG to JPG', command=convert, bg='royalblue', fg='black', font=('georgia', 14, 'bold'))
+savebutton = tk.Button(text = 'Convert PNG to JPG', command=convert, bg='purple1', fg='white', font=('georgia', 14, 'bold'))
 canvas01.create_window(230, 270, window=savebutton)
 root.mainloop()
